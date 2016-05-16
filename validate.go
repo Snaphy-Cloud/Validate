9fe1307e7de405c8e68eb27134d2a9e6de26b56a
robinskumar73 <robinskumar73@gmail.com>
2016-05-16 19:40:50 +0530

    First commit

---
+++ validate.go
@@ -0,0 +1,27 @@
+package Validate
+
+import "fmt"
+
+//Defigning a vlidate function which return a isValid and message
+type  Validate func(value string) (message string, isValid bool)
+
+//Accept as value from clid and validate func which must return true to proceed..
+/*
+    Example
+    GetInput(&id, "Enter user Id: ", func(value string)(message string, isValid bool){
+	return "User Id must be a number", IsNumeric(id)
+
+    })
+ */
+func GetInput(value *string, message string,  validFn Validate){
+	isValid := false
+	var validMsg string
+	for !isValid{
+		fmt.Println(message)
+		fmt.Scanln(value)
+		validMsg, isValid = validFn(*value)
+		if !isValid{
+			fmt.Println(validMsg)
+		}
+	}
+}
